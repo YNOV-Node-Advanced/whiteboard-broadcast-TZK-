1a94eb97dc549bebfa908d1b49673c3887cb0eff
Thibault <thib-17@orange.fr>
2018-05-31 14:21:38 +0200

    Websocket server

--- server/index.js
+++ server/index.js
@@ -1,14 +1,24 @@
 const path = require("path");
 const express = require("express");
 const uuidv4 = require("uuid/v4");
+const ws = require("ws");
 
 const app = express();
 
 const PUBLIC_FOLDER = path.join(__dirname, "../public");
 const PORT = process.env.PORT || 5000;
 
-// Completer ce fichier
-
+const wss = new ws.Server({ port: 8081 });
+wss.on("connection", function connection(ws) {
+    ws.on("message", function incoming(data) {
+        console.log("received: %s", data);
+        wss.clients.forEach(client => {
+            if (client !== ws && client.readyState === ws.OPEN) {
+                client.send(data);
+            }
+        });
+    });
+});
 
 // Assign a random channel to people opening the application
 app.get("/", (req, res) => {
@@ -24,3 +34,5 @@ app.get("/:channel", (req, res, next) => {
 });
 
 app.use(express.static(PUBLIC_FOLDER));
+
+app.listen(PORT, () => console.log(`Listening on port ${PORT}!`));
